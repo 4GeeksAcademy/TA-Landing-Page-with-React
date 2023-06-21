9b996b612d5126690031dfb0792e31a9df303579
Tcm68 <131905512+Tcm68@users.noreply.github.com>
2023-06-21 23:55:15 +0000

    first commit

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Navbar = () => {
+  const navbarStyle = {
+    backgroundColor: '#696969',
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg" style={navbarStyle}>
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">
+          Start Bootstrap
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
