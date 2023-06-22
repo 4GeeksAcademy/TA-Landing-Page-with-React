11ec6e29120fdbeea6fdd1139f3cdbe061efae10
Xiang Zheng <68246040+archmyth@users.noreply.github.com>
2023-06-22 03:55:07 +0000

    merge jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from './Navbar.jsx';
-
-
+import Jumbotron from "./Jumbotron.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
+			<Jumbotron />
 			
 		</div>
 			
